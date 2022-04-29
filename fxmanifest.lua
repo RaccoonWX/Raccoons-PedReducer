4e5a48ba60bb1bcdb58157fe11d6bc6933aad8bd
TheRaccoon <hello@theraccoon.dev>
2022-04-29 00:39:51 -0500

    Resolved game not supported
    Resolved an issue where the server console, displayed the script not supported with GTA 5.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version "cerulean"
-games "gta5"
+games { 'gta5' }
 
 name "Raccoons PedReducer"
 author "TheRaccoon#1023 (PeEcEeChIP)"
 description "Created by TheRaccoon, for reducing pedestrian density around in the city. | Discord: https://discord.gg/7S2y2hrXQj"
-version "v1.0.0"
+version "v1.0.0.1"
 
 client_scripts "/client/PedDensity.lua"
 server_script "server/verion_check.lua"
